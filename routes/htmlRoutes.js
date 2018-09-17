9957fc736c8e9d13781bec01106d80900d7c3e66
Sajeel Malik <sajeel23@berkeley.edu>
2018-09-17 14:35:22 -0700

    add vote and save routing

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,22 @@
+// DEV LAB: htmlRoutes.js - this file offers a set of routes for sending users to the various html pages
+// *********************************************************************************
+// Dependencies
+// =============================================================
+var path = require("path");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  // index route loads index.html
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+
+  // All educational resources in resources.html
+  // app.get("/resources", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/resources.html"));
+  // });
+
+ 
+};
