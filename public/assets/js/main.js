cb4d512b492275ed520a334a0aaf047a7d4e720c
BQ <qian.brian@gmail.com>
2018-09-20 09:47:17 -0700

    function created to create user content

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -21,9 +21,14 @@ $(function () {
         $("#logout-link").hide();
 
         $("#add-content-button").hover(function () {
-            console.log("test");
-            $("#add-content-button").text("Login to DevLab to add your favorite resources!");
-        })
+                var $this = $(this); // caching $(this)
+                $this.data('initialText', $this.text());
+                $this.text("Login to DevLab to add your favorite resources!");
+            },
+            function () {
+                var $this = $(this); // caching $(this)
+                $this.text($this.data('initialText'));
+            });
     }
 
     //PAGE LOGIN
@@ -34,28 +39,28 @@ $(function () {
         console.log("sign in" + email);
         $.ajax({
             type: "POST",
-            url: '/signin', 
+            url: '/signin',
             data: {
-            email: email,
-            password: password
+                email: email,
+                password: password
             },
-            success: function() {   
-                location.reload();  
-            }
-        })
-    })
-
-    $(document).on("click", "#logout-link", function(e){
-        $.ajax({
-            type: "GET",
-            url: "/signout",
-            success: function(){
+            success: function () {
                 location.reload();
             }
         })
     })
 
-    //POPULATES CONCEPT CATEGORIES
+    $(document).on("click", "#logout-link", function (e) {
+        $.ajax({
+            type: "GET",
+            url: "/signout",
+            success: function () {
+                location.reload();
+            }
+        })
+    })
+
+    //POPULATES CONCEPT CATEGORIES ON PAGE LOAD
     $.get('/api/contents', function (err, data) {
             if (err) throw err;
             console.log(data);
@@ -70,6 +75,7 @@ $(function () {
                 $(".concept-container").append(newDiv);
                 dropdownOption.text(concept.conceptTitle);
                 $('#new-concept').append(dropdownOption);
+                $('#user-category-dropdown').append(dropdownOption);
 
             });
         })
@@ -115,6 +121,8 @@ $(function () {
             })
         })
     })
+
+
 
     //ON CLICK LISTENER FOR SAVING CONTENT
     $(document).on('click', ".star-image", function (e) {
@@ -179,88 +187,92 @@ $(function () {
                 //CREATE USER CONTENT DIVS
                 $.get(`/api/users/${userID}`, function (err, data) {
                     if (err) throw err;
-                }).then(data => {
-                    userContentArray = [];
-                    data.savedLinks.forEach(elem => {
-                        userContentArray.push(elem.id);
-                        var dropdownOption = $(`<option value=${elem.conceptTitle}>`);
-                        dropdownOption.text(`${elem.conceptTitle}`);
-                        $("#user-category-dropdown").append(dropdownOption);
-                        var userContainer = $("<div class='user-content-container'>");
-                        var userTitle = $("<div class='user-title'>");
-                        var userSaves = $(`<span class='star-number'>${elem.saves}</span>`);
-                        var userDate = $("<div class='user-date'>");
-                        var userImage = $(`<i data-id='${elem.id}' data-value='${elem.saves}'class='fas fa-star star-image'></i>`);
-                        if (userContentArray.includes(elem.id)) userImage.addClass('saved');
-                        userTitle.text(elem.contentTitle);
-                        userSaves.append(userImage);
-                        var createdDate = elem.createdAt;
-                        userDate.append(moment(createdDate).format('MM DD YYYY'))
-                        $(userContainer).append(userTitle, userSaves, userDate);
-                        $(".user-library").append(userContainer);
-                    })
-
-
-                })
-            }
-        })
-    }
-    $("#full-library-link").on('click', function () {
-
-        getCurrentSaves();
-        if (!$(this).hasClass('active')) {
-            userID ? $("#landing").hide() : $("#landing").show();
-
-            $(this).toggleClass('active')
-            $(".user-library").empty();
-            $("#user-category-dropdown").empty();
-            $(".uk-button-danger").show();
-            $(".uk-divider-icon").show();
-            $("#user-library-link").toggleClass('active')
-            $(".library-container").show();
-            $(".add-content").show();
-            $(".user-library-container").css('display', 'none')
-        }
-    })
-
-    $("#sign-up-button").on('click', function (e) {
-        $(".sign-up-modal").css('display', 'flex');
-        $(".screen-overlay").css('display', 'flex');
-    });
-    $("#sign-up-cancel").on('click', function (e) {
-        $(".sign-up-modal").css('display', 'none');
-        $(".screen-overlay").css('display', 'none');
-    });
-    $("#sign-up-submit").on('click', function (e) {
-        e.preventDefault();
-        var newUser = {
-            name: $("input[name=userName]").val(),
-            password: $("input[name=userPassword]").val(),
-            email: $("input[name=userEmail]").val()
-        }
-        $.ajax({
-            type: "POST",
-            url: '/signup', 
-            data: newUser,
-            success: function() {   
-                location.reload();  
+                }).then(createUserLibrary(data))
             }
         })
 
-
-        console.log(newUser);
-    })
-
-    function getCurrentSaves() {
-        $.get(`/api/users/${userID}`, function (err, data) {
-            if (err) throw err;
-        }).then(resp => {
-            userContentArray = [];
-            resp.savedLinks.forEach(elem => {
-                userContentArray.push(elem.id)
-            })
+        $("#sort-date-ASC").on('click', function(){
+            $.get(`/api/users/${userID}/dateD`, function (err, data){
+                if (err) throw err;
+            }).then(createUserLibrary(data))
         })
     }
+
+    function createUserLibrary(data) {
+
+        $(".user-library").empty();
+        userContentArray = [];
+        data.savedLinks.forEach(elem => {
+            userContentArray.push(elem.id);
+            var userContainer = $("<div class='user-content-container'>");
+            var userTitle = $("<div class='user-title'>");
+            var userSaves = $(`<span class='star-number'>${elem.saves}</span>`);
+            var userDate = $("<div class='user-date'>");
+            var userImage = $(`<i data-id='${elem.id}' data-value='${elem.saves}'class='fas fa-star star-image'></i>`);
+            if (userContentArray.includes(elem.id)) userImage.addClass('saved');
+            userTitle.text(elem.contentTitle);
+            userSaves.append(userImage);
+            var createdDate = elem.createdAt;
+            userDate.append(moment(createdDate).format('MM DD YYYY'))
+            $(userContainer).append(userTitle, userSaves, userDate);
+            $(".user-library").append(userContainer);
+        })
+    }
+
+$("#full-library-link").on('click', function () {
+
+    getCurrentSaves();
+    if (!$(this).hasClass('active')) {
+        userID ? $("#landing").hide() : $("#landing").show();
+
+        $(this).toggleClass('active')
+        $(".user-library").empty();
+        $("#user-category-dropdown").empty();
+        $(".uk-button-danger").show();
+        $(".uk-divider-icon").show();
+        $("#user-library-link").toggleClass('active')
+        $(".library-container").show();
+        $(".add-content").show();
+        $(".user-library-container").css('display', 'none')
+    }
+})
+
+$("#sign-up-button").on('click', function (e) {
+    $(".sign-up-modal").css('display', 'flex');
+    $(".screen-overlay").css('display', 'flex');
+}); $("#sign-up-cancel").on('click', function (e) {
+    $(".sign-up-modal").css('display', 'none');
+    $(".screen-overlay").css('display', 'none');
+}); $("#sign-up-submit").on('click', function (e) {
+    e.preventDefault();
+    var newUser = {
+        name: $("input[name=userName]").val(),
+        password: $("input[name=userPassword]").val(),
+        email: $("input[name=userEmail]").val()
+    }
+    $.ajax({
+        type: "POST",
+        url: '/signup',
+        data: newUser,
+        success: function () {
+            location.reload();
+        }
+    })
+
+
+    console.log(newUser);
+})
+
+function getCurrentSaves() {
+    $.get(`/api/users/${userID}`, function (err, data) {
+        if (err) throw err;
+    }).then(resp => {
+        userContentArray = [];
+        resp.savedLinks.forEach(elem => {
+            userContentArray.push(elem.id)
+        })
+    })
+}
 
 
 })
